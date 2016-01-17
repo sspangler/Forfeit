c5294a983a29081bf6c0202e023c0e2a2f719115
sspangler <sspangler@ltu.edu>
2016-01-17 01:38:58 -0500

    Back up for crash

--- Senior Project/Assets/LevelGenerator.cs
+++ Senior Project/Assets/LevelGenerator.cs
@@ -3,38 +3,115 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour {
+	public int smallDim;
 
 	public List<GameObject> Rooms = new List<GameObject>();
-
+	
 	public int levelColumns;
 	public int levelRows;
 	Vector3 startPos = Vector3.zero;
-	Vector3 nextYPos;
-	Vector3 nextXPos;
+	Vector3 nextPos;
 	int rowCounter;
-	Vector3 topRight;
+
+	LayerMask roomParentLayer;
+
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < levelColumns; i++) {
-			RaycastHit2D hit = Physics2D.Raycast (topRight, Vector2.right, 1f);
-			if (!hit) { //didnt hit something
-				
-				int levelPiece = Random.Range (0, Rooms.Count - 1);
-				GameObject nextLevelPiece = (GameObject)Instantiate (Rooms [levelPiece], startPos + nextXPos, Quaternion.identity);
-				nextXPos += Vector3.right * nextLevelPiece.transform.GetChild (0).localScale.x;
 
-				if (i == levelColumns - 1 && rowCounter != levelRows - 1) {
-					nextXPos = (startPos + (Vector3.down * (rowCounter + 1))) * nextLevelPiece.transform.localScale.x;
-					i = -1;
-					rowCounter += 1;
-				}
-			} else { //hit something
-				nextXPos += Vector3.right * hit.transform.localScale.x;
+		roomParentLayer = 1 << LayerMask.NameToLayer ("RoomParent");
+
+		for (int i = 0; i < levelColumns; i++) {
+
+			ChooseNextPiece();
+
+//			RaycastHit2D hit = Physics2D.Raycast(nextPos * 1.001f, Vector2.right, .1f, roomParentLayer);
+//			if (hit.collider != null) {
+//				print(hit.transform.name + "  " + i);
+//			} else {
+//				print ("nothing " + i);
+//			}
+
+	
+			if (i == levelColumns - 1 && rowCounter != levelRows - 1) {
+				nextPos = startPos + Vector3.down * smallDim;
+				i = -1;
+				rowCounter += 1;
 			}
 		}
 	}
-
-	void OnLevelWasLoaded () {
+	
+	void ChooseNextPiece () {
+		int levelPiece = Random.Range (0, Rooms.Count - 1);
+		print (Rooms [levelPiece].tag);
+		if (Rooms [levelPiece].tag == "SmallRoom") {
+			if (CheckSmall ()) {
+				CreatePiece(levelPiece);
+			} else {
+				nextPos += Vector3.right * smallDim;
+				ChooseNextPiece();
+			}
+		} else if (Rooms [levelPiece].tag == "BigRoom") {
+			if (CheckLarge ()) {
+				CreatePiece(levelPiece);
+			} else {
+				ChooseNextPiece();
+			}
+		} else if (Rooms [levelPiece].tag == "VertRoom") {
+			if (CheckVert ()) {
+				CreatePiece(levelPiece);
+			} else {
+				ChooseNextPiece();
+			}
+		} else if (Rooms [levelPiece].tag == "HorzRoom") {
+			if (CheckHorz ()) {
+				CreatePiece(levelPiece);
+			} else {
+				ChooseNextPiece();
+			}
+		}
 		
+	}
+	
+	bool CheckSmall () {
+		RaycastHit2D hit = Physics2D.Raycast(nextPos * 1.001f, Vector2.right, 1f, roomParentLayer);
+		if (hit.collider == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	bool CheckLarge () {
+		RaycastHit2D hitTop = Physics2D.Raycast(nextPos * 1.001f, Vector2.right, 1f, roomParentLayer);
+		RaycastHit2D hitBottom = Physics2D.Raycast(nextPos * 1.001f, Vector2.right, 1f, roomParentLayer);
+		if (hitTop.collider == null && hitBottom.collider == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	bool CheckVert () {
+		RaycastHit2D hitTop = Physics2D.Raycast(nextPos * 1.001f, Vector2.right, 1f, roomParentLayer);
+		RaycastHit2D hitBottom = Physics2D.Raycast(nextPos * 1.001f, Vector2.right, 1f, roomParentLayer);
+		if (hitTop.collider == null && hitBottom.collider == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	bool CheckHorz () {
+		RaycastHit2D hitTop = Physics2D.Raycast(nextPos * 1.001f, Vector2.right, 1f, roomParentLayer);
+		if (hitTop.collider == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	void CreatePiece (int roomPiece) {
+		GameObject nextLevelPiece = (GameObject)Instantiate (Rooms [roomPiece], startPos + nextPos, Quaternion.identity);
+		nextPos += Vector3.right * nextLevelPiece.transform.localScale.x;
 	}
 }
