dc06e897b5bb783f3fe00613d6a9b612488497e3
sspangler <sspangler@ltu.edu>
2016-02-02 03:06:56 -0500

    Mass damage started camera moved farther away from player

--- Senior Project/Assets/Scripts/Player/Actives/Invincibility.cs
+++ Senior Project/Assets/Scripts/Player/Actives/Invincibility.cs
@@ -5,7 +5,7 @@ public class Invincibility : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
--- Senior Project/Assets/Scripts/Player/Actives/MassDamage.cs
+++ Senior Project/Assets/Scripts/Player/Actives/MassDamage.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MassDamage : MonoBehaviour {
 
+	public List<GameObject> enemiesInRange = new List<GameObject>();
+	public BoxCollider2D col;
+
 	// Use this for initialization
 	void Start () {
-	
+		col = gameObject.AddComponent<BoxCollider2D> ();
+		col.isTrigger = true;
+		col.enabled = false;
+		col.size = new Vector2 (5, 5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.E)) {
+			col.enabled = true;	
+			BlowShitUp ();
+		}
+	}
+
+	void BlowShitUp () {
+		foreach (GameObject enemy in enemiesInRange) {
+			Destroy (enemy.gameObject, .1f);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D enemyCol) {
+		if (enemyCol.tag == "Enemy")
+			enemiesInRange.Add (enemyCol.gameObject);
+	}
+
+	void OnTriggerExit2D (Collider2D enemyCol) {
+		if (enemyCol.tag == "Enemy")
+			enemiesInRange.Remove (enemyCol.gameObject);
 	}
 }
