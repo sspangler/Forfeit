ed5623962c5759ff7e756d6892c81084d1b00a50
sspangler <sspangler@ltu.edu>
2016-02-12 00:57:06 -0500

    Debuffer enemy and debuff script

--- Senior Project/Assets/Scripts/Enemy/SpeedDebuff.cs
+++ Senior Project/Assets/Scripts/Enemy/SpeedDebuff.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class SpeedDebuff : MonoBehaviour {
 
+	public float duration;
+
+	public float intesity;
+
 	// Use this for initialization
 	void Start () {
-	
+		GetComponent<PlayerController> ().speed = GetComponent<PlayerController> ().speed * .75f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		duration -= Time.deltaTime;
+
+		if (duration < 0) {
+			GetComponent<PlayerController> ().speed = GetComponent<PlayerController> ().speed / .75f;
+			Destroy (this);
+		}
 	}
 }
