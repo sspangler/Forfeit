c6f723911dffd571b46519422ae4f6e96e0fa365
sspangler <sspangler@ltu.edu>
2016-02-13 23:33:04 -0500

    lobber started

--- Senior Project/Assets/Scripts/Enemy/ProjectileLobber.cs
+++ Senior Project/Assets/Scripts/Enemy/ProjectileLobber.cs
@@ -2,10 +2,17 @@
 using System.Collections;
 
 public class ProjectileLobber : MonoBehaviour {
-	
+	public GameObject projectile;
+
+
+	public float vertForce;
+	public float horzForce;
+
+	Vector3 direction;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
